0fc75b161d7e07079e12dc74c319b45d30ed3336
Lonkmaster <link2040abcd@gmail.com>
2024-05-14 17:58:17 -0500

    Filled out the rest of the page with the html and css

---
+++ javascript.js
@@ -0,0 +1,7 @@
+const password = document.querySelector("#password")
+const confirmPassword = document.querySelector("#confirmPassword")
+const form = document.querySelector("form")
+
+function validateForm() {
+    let x = document.forms["myForm"]
+}
